f888292b8247529f5301fbde395c76a3bdccc33c
maya <mayanb@gmail.com>
2018-02-05 18:25:27 -0800

    fix search from crashing

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -149,7 +149,12 @@ class Search extends Component {
           success(found, semantic)
 
           // it should only do this if found != null
-          this.navigateToFoundTask(found.creating_task)
+          if(found != null ) {
+            this.navigateToFoundTask(found.creating_task)
+          } else {
+            //TODO: should bring up a dialog saying this QR code isn't in our system
+            alert("nope")
+          }
         }
       })
   }
