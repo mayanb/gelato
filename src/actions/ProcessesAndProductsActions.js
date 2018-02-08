df28091c0120902103c02b78aa1034146ba16301
Ishita Prasad <ishita@usepolymer.com>
2018-02-07 18:14:48 -0800

    tabs

--- src/actions/ProcessesAndProductsActions.js
+++ src/actions/ProcessesAndProductsActions.js
@@ -2,106 +2,106 @@ import Storage from '../resources/Storage'
 import Networking from '../resources/Networking-superagent'
 import Compute from '../resources/Compute'
 import {
-  REQUEST,
-  REQUEST_SUCCESS,
-  REQUEST_FAILURE,
+	REQUEST,
+	REQUEST_SUCCESS,
+	REQUEST_FAILURE,
 } from '../reducers/BasicReducer'
 
 const PROCESSES = 'PROCESSES'
 const PRODUCTS = 'PRODUCTS'
 
 export function fetchProcesses() {
-  return fetch(PROCESSES)
+	return fetch(PROCESSES)
 }
 
 export function fetchProducts() {
-  return fetch(PRODUCTS)
+	return fetch(PRODUCTS)
 }
 
 function fetch(name) {
-  let endpoint = name === PROCESSES ? 'processes' : 'products'
-  return dispatch => {
-    dispatch(request(name))
+	let endpoint = name === PROCESSES ? 'processes' : 'products'
+	return dispatch => {
+		dispatch(request(name))
 
-    let team = 1
-    return Storage.multiGet(['teamID', 'userID']).then(values => {
-      let localStorage = {}
-      values.forEach((element, i) => {
-        let key = element[0]
-        let val = element[1]
-        localStorage[key] = val
-      })
-      team = localStorage['teamID']
-       return Networking.get(`/ics/${endpoint}/`)
-        .query({ team_created_by: team })
-        .then(res => {
-          dispatch(requestSuccess(name, res.body))
-        })
-        .catch(e => {
-          dispatch(requestFailure(name, e))
-          throw e
-        })
-    })
-  }
+		let team = 1
+		return Storage.multiGet(['teamID', 'userID']).then(values => {
+			let localStorage = {}
+			values.forEach((element, i) => {
+				let key = element[0]
+				let val = element[1]
+				localStorage[key] = val
+			})
+			team = localStorage['teamID']
+			 return Networking.get(`/ics/${endpoint}/`)
+				.query({ team_created_by: team })
+				.then(res => {
+					dispatch(requestSuccess(name, res.body))
+				})
+				.catch(e => {
+					dispatch(requestFailure(name, e))
+					throw e
+				})
+		})
+	}
 }
 
 function request(name) {
-  return {
-    type: REQUEST,
-    name: name,
-  }
+	return {
+		type: REQUEST,
+		name: name,
+	}
 }
 
 function requestSuccess(name, data) {
-  return {
-    type: REQUEST_SUCCESS,
-    name: name,
-    data: data,
-  }
+	return {
+		type: REQUEST_SUCCESS,
+		name: name,
+		data: data,
+	}
 }
 
 function requestFailure(name, err) {
-  return {
-    type: REQUEST_FAILURE,
-    name: name,
-    error: err,
-  }
+	return {
+		type: REQUEST_FAILURE,
+		name: name,
+		error: err,
+	}
 }
 
 export function updateAttribute(task, attribute_id, new_value) {
-  console.log('is this even being used')
-  let name = task.is_open ? OPEN_TASKS : COMPLETED_TASKS
-  return dispatch => {
-    let payload = {
-      task: task.id,
-      attribute: attribute_id,
-      value: new_value,
-    }
+	console.log('is this even being used')
+	let name = task.is_open ? OPEN_TASKS : COMPLETED_TASKS
+	return dispatch => {
+		let payload = {
+			task: task.id,
+			attribute: attribute_id,
+			value: new_value,
+		}
 
-    return Networking.post('/ics/taskAttributes/create/')
-      .send(payload)
-      .end(function(err, res) {
-        if (err || !res.ok) {
-          dispatch(updateAttributeFailure(name, err))
-        } else {
-          dispatch(updateAttributeSuccess(name, res.body))
-        }
-      })
-  }
+		return Networking.post('/ics/taskAttributes/create/')
+			.send(payload)
+			.end(function(err, res) {
+				if (err || !res.ok) {
+					dispatch(updateAttributeFailure(name, err))
+				} else {
+					dispatch(updateAttributeSuccess(name, res.body))
+				}
+			})
+	}
 }
 
 function updateAttributeSuccess(name, data) {
-  return {
-    name: name,
-    type: UPDATE_ATTRIBUTE_SUCCESS,
-    data: data,
-  }
+	return {
+		name: name,
+		type: UPDATE_ATTRIBUTE_SUCCESS,
+		data: data,
+	}
 }
 
 function updateAttributeFailure(name, err) {
-  return {
-    name: name,
-    type: UPDATE_ATTRIBUTE_FAILURE,
-    error: err,
-  }
+	return {
+		name: name,
+		type: UPDATE_ATTRIBUTE_FAILURE,
+		error: err,
+	}
 }
