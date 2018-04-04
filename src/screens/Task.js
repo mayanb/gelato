29931ad216a54742effa291b45ce1cb458aa4573
David Mora <davidnmora@gmail.com>
2018-04-03 18:36:38 -0700

    Update Input help instructions

--- src/screens/Task.js
+++ src/screens/Task.js
@@ -165,10 +165,9 @@ class Task extends Component {
 
 	showHelpAlert() {
 		AlertIOS.alert(
-			'Where do I see the scanned QR codes?',
-			`** Tap the QR code button on the bottom right 
-			\n ** Select inputs or outputs 
-			\n ** Tap the button with the # of scanned codes on the bottom left of the camera screen`
+			'Where do I see already scanned QR codes for this task?',
+			`** Tap the blue input button on the bottom right
+			\n ** Then tap the button with the # of scanned codes on the bottom left of the camera screen`
 		)
 	}
 
