a3e1c91e38b0792f05821c16d005848715f71aa3
Ishita Prasad <ishita@usepolymer.com>
2018-04-11 12:53:04 -0700

    clean up

--- src/components/CreateTask/SelectTypeWithInput.js
+++ src/components/CreateTask/SelectTypeWithInput.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
-import { Dropdown } from '../Dropdown'
-import { SelectTypeInput, CreateTaskCell } from './CreateTaskCell'
+import {
+	View,
+	StyleSheet,
+	TouchableWithoutFeedback,
+	Image,
+	Text,
+	TextInput,
+} from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import Colors from '../../resources/Colors'
+import * as ImageUtility from '../../resources/ImageUtility'
 
-export default class SelectTypes extends Component {
+export default class SelectTypeWithInput extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
@@ -16,28 +23,83 @@ export default class SelectTypes extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<SelectTypeInput
+				<EditableCell
 					placeholder={this.props.placeholder}
 					onChangeText={t => this.props.onChangeText(t)}
-					returnKeyType={this.props.done ? "done" : "next"}
+					returnKeyType={this.props.done ? 'done' : 'next'}
 					blurOnSubmit={false}
 					value={this.props.text}
 					selected={this.props.selected}
 					registerInput={this.props.registerInput}
 					onFocus={this.props.onFocus}
 				/>
-				{ this.props.dropdown_open && (
-				<KeyboardAwareScrollView keyboardShouldPersistTaps='handled' >
-					{this.props.data.map(e => {
-						return <CreateTaskCell key={'stwi-' + e.id} {...e} imgpath={e.icon} onPress={() => this.props.onSelect(e)}/>
-					})}
-				</KeyboardAwareScrollView>
+				{this.props.dropdown_open && (
+					<KeyboardAwareScrollView keyboardShouldPersistTaps="handled">
+						{this.props.data.map(e => {
+							return (
+								<NonEditableCell
+									key={'stwi-' + e.id}
+									{...e}
+									imgpath={e.icon}
+									onPress={() => this.props.onSelect(e)}
+								/>
+							)
+						})}
+					</KeyboardAwareScrollView>
 				)}
 			</View>
 		)
 	}
 }
 
+export function EditableCell({ style, selected, registerInput, ...rest }) {
+	let img = selected ? ImageUtility.requireIcon(selected.icon) : ImageUtility.systemIcon('unknown')
+	return (
+		<View style={styles.cell_container}>
+			<Image
+				source={img}
+				style={styles.process_icon}
+			/>
+			<TextInput
+				style={[style, styles.input]}
+				autoCapitalize="none"
+				autoCorrect={false}
+				underlineColorAndroid="transparent"
+				ref={input => registerInput(input)}
+				{...rest}
+			/>
+		</View>
+	)
+}
+
+export class NonEditableCell extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		let { onPress, imgpath, name } = this.props
+		return (
+			<TouchableWithoutFeedback activeOpacity={0.5} onPress={onPress}>
+				<View style={styles.cell_container}>
+					<Image
+						source={ImageUtility.requireIcon(imgpath)}
+						style={styles.process_icon}
+					/>
+					<Text style={styles.display}>{name}</Text>
+					{this.props.header ? (
+						<Image
+							style={styles.arrow}
+							source={ImageUtility.requireIcon('downarrow.png')}
+						/>
+					) : null}
+				</View>
+			</TouchableWithoutFeedback>
+		)
+	}
+}
+
+const imgSize = 24
 const styles = StyleSheet.create({
 	container: {
 		marginTop: 20,
@@ -60,5 +122,36 @@ const styles = StyleSheet.create({
 		paddingBottom: 40,
 		flex: 1,
 		backgroundColor: 'blue',
+	},
+	cell_container: {
+		paddingTop: 8,
+		paddingBottom: 8,
+		paddingLeft: 20,
+		paddingRight: 20,
+		borderBottomStyle: 'solid',
+		borderBottomRadius: 4,
+		borderBottomWidth: 1,
+		borderColor: 'rgba(0, 0, 0, 0.08)',
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+	}, 
+	process_icon: {
+		width: imgSize,
+		height: imgSize,
+		marginRight: 8,
+		flexGrow: 0,
+	},
+	input: {
+		flex: 1,
+		height: 40,
+		fontSize: 17,
+		color: Colors.gray,
+
+	},
+	display: {
+		fontSize: 17,
+		color: Colors.textblack,
+		flex: 1,
 	},
 })
