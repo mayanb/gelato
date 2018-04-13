99febfc49cca23559c576e42f0e64d79cd3573b7
Steven Guichard <steven@usepolymer.com>
2018-04-12 17:45:35 -0700

    animation fixes

--- src/components/Ingredients/PanelExpander.js
+++ src/components/Ingredients/PanelExpander.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, TouchableWithoutFeedback, Animated, Image } from 'react-native'
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Animated, Image, Dimensions } from 'react-native'
 import * as ImageUtility from '../../resources/ImageUtility'
 
 import Colors from '../../resources/Colors'
+
+const CLOSED_PERCENTAGE = 0.2
+const OPEN_PERCENTAGE = 0.8
+const WINDOW_HEIGHT = Dimensions.get('window').height
 
 export default class PanelExpander extends Component {
 
@@ -10,7 +14,7 @@ export default class PanelExpander extends Component {
 		super(props)
 		this.state = {
 			open: false,
-			panelHeight: new Animated.Value(0.2),
+			panelHeight: new Animated.Value(0),
 		}
 
 		this.handleOpen = this.handleOpen.bind(this)
@@ -26,10 +30,11 @@ export default class PanelExpander extends Component {
 	}
 
 	handleClose() {
+		this.setState({ open: false })
 		Animated.timing(this.state.panelHeight, {
-			toValue: 0.2,
+			toValue: 0,
 			duration: 400,
-		}).start(() => this.setState({ open: false }))
+		}).start()
 	}
 
 	render() {
@@ -40,12 +45,21 @@ export default class PanelExpander extends Component {
 				justifyContent: 'flex-end',
 			},
 		})
+		const flex = this.state.panelHeight.interpolate({
+			inputRange: [0, 1],
+			outputRange: [CLOSED_PERCENTAGE, OPEN_PERCENTAGE]
+		})
+
 		return (
 			<View style={styles.container}>
 				{this.state.open && <Overlay onClose={this.handleClose} />}
 				{camera}
-				<Animated.View style={{ flex: this.state.panelHeight }}>
-					<ArrowOverlay open={this.state.open} onOpen={this.handleOpen} onClose={this.handleClose} />
+				<Animated.View style={{
+					flex: flex,
+					maxHeight: OPEN_PERCENTAGE * WINDOW_HEIGHT,
+				}}>
+					<ArrowOverlay open={this.state.open} onOpen={this.handleOpen} onClose={this.handleClose}
+					              animateOpen={this.state.panelHeight} />
 					<IngredientsContainer onOpen={this.handleOpen} content={ingredientsContent} />
 				</Animated.View>
 			</View>
@@ -58,17 +72,20 @@ function Overlay({ onClose }) {
 		<TouchableWithoutFeedback onPress={onClose}>
 			<View
 				style={{
-					flex: 0.2,
+					flex: CLOSED_PERCENTAGE,
 					zIndex: 10,
-					backgroundColor: Colors.black,
-					opacity: 0.8,
 				}}
 			/>
 		</TouchableWithoutFeedback>
 	)
 }
 
-function ArrowOverlay({ open, onOpen, onClose }) {
+function ArrowOverlay({ open, onOpen, onClose, animateOpen }) {
+	const spin = animateOpen.interpolate({
+		inputRange: [0, 1],
+		outputRange: ['0deg', '180deg']
+	})
+
 	const styles = StyleSheet.create({
 		container: {
 			height: 36,
@@ -78,15 +95,15 @@ function ArrowOverlay({ open, onOpen, onClose }) {
 		image: {
 			height: 36,
 			width: 36,
-			transform: [{ rotate: open ? '180deg' : '0deg' }],
+			transform: [{ rotate: spin }],
 		},
 	})
 	return (
 		<TouchableWithoutFeedback onPress={open ? onClose : onOpen}>
-			<View
+			<Animated.View
 				style={styles.container}>
-				<Image source={ImageUtility.requireIcon('uparrowwhite.png')} style={styles.image} />
-			</View>
+				<Animated.Image source={ImageUtility.requireIcon('uparrowwhite.png')} style={styles.image} />
+			</Animated.View>
 		</TouchableWithoutFeedback>
 	)
 }
