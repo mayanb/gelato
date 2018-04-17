06348c4e10675946168ee4116d16b350939d0a46
Ishita Prasad <ishitap@stanford.edu>
2018-04-17 11:21:46 -0700

    Update urls.js

--- src/resources/urls.js
+++ src/resources/urls.js
@@ -13,7 +13,7 @@ export function getBackend() {
 			return STAGING_URL
 		default:
 			// change this if you want to change what your dev app is using
-			return PRODUCTION_URL
+			return STAGING_URL
 			// return LOCAL_URL
 	}
 }
