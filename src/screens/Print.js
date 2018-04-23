5c96750ad4941d6c61da89633ce424acb22e6c31
Ishita Prasad <ishita@usepolymer.com>
2018-04-23 09:32:21 -0700

    printed label styling

--- src/screens/Print.js
+++ src/screens/Print.js
@@ -7,6 +7,79 @@ import { DangerZone } from 'expo'
 import QRCode from 'qrcode'
 import uuid from 'uuid/v4'
 import paramsToProps from '../resources/paramsToProps'
+import moment from 'moment'
+
+function generateHTML(svg, task) {
+	let { process_type, product_type, display, created_at } = task
+	let m = 1.2
+
+	// adjust m for scaling, adjust .containerouter height and width for label size 
+	let styles = `<style>
+	.containerouter {
+		min-height: 60vh; 
+  	min-width: 100vw;
+  	height: 60vh;
+  	width: 100vw;
+	}
+	.container {
+	min-width: ${420*m}px;
+  min-height: ${300*m}px;
+  width: ${420*m}px;
+  height: ${300*m}px;
+  font-family: monospace;
+  font-size: ${13*m}px;
+  transform: rotate(90deg); 
+  transform-origin: top left;
+  margin-left: ${252*m}px;
+}
+
+.main {
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+}
+
+.info-extended {
+  margin-top: ${13*m}px;
+}
+
+.info-extended > span {
+  display: block;
+  font-size: ${19*m}px;
+  font-weight: bold;
+}
+
+.info-extended > span.info-words {
+   font-weight: normal;
+   font-size: ${15*m}px;
+   display: block;
+   margin-top: ${13*m}px;
+} 
+
+.lot-code {
+  font-size: ${25*m}px;
+  margin-left: ${13*m}px;
+  font-weight: bold;
+  
+}</style>`
+	return `${styles}<div class='containerouter'><div class='container'>
+	  <div class='main'>
+	     ${svg}
+	     <div class='info-extended'>
+	       <span>${process_type.code}</span>
+	       <span>${product_type.code}</span>
+	       <span>${moment(created_at).format('MMM DD YYYY')}</span>
+	       <span class="info-words">${process_type.name} ${product_type.name}</span>
+	     </div>
+	  </div>
+	  <div class='bar'>
+	    ---LOT CODE-----------------------------
+	  </div>
+	  <div class='lot-code'>
+	    ${display}
+	  </div>
+	</div></div>`
+}
 
 class Print extends Component {
 	static navigationOptions = {
@@ -34,31 +107,15 @@ class Print extends Component {
   }
 
 	generateQRCode(data, qrdocument) {
-		console.log('hi')
-		let text = `
-      <style>
-        #rotate-text { 
-          margin-left: 100px; 
-          margin-bottom: 200px; 
-          width: 200px; 
-          transform: rotate(90deg); 
-          transform-origin: top left; 
-          font-size:30px;
-        }
-      </style>
-      <div id="rotate-text"><p>${this.props.selectedTask.display}</p></div>`
 		return new Promise((resolve, reject) => {
-			QRCode.toString(data, function(err, string) {
+			QRCode.toString(data, (err, string) => {
 				if (err) {
 					console.log('error')
 					console.log(err)
 					reject(err)
 				} else {
-					let updatedHTML =
-						string.slice(0, 4) +
-						` height="130px" width="130px"` +
-						string.slice(4)
-					updatedHTML += text
+					let updatedSVG = string.slice(0, 4) + ` height="204px" ` + string.slice(4)
+					let updatedHTML = generateHTML(updatedSVG, this.props.selectedTask)
 					qrdocument.push(updatedHTML)
 					resolve(updatedHTML)
 				}
