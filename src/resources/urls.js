f4e5f3372ac4690169b73e9735ab53f0b1f82ea6
David Mora <davidnmora@gmail.com>
2018-05-08 14:24:55 -0400

    Remove specific IP address from LOCAL_URL

--- src/resources/urls.js
+++ src/resources/urls.js
@@ -2,7 +2,7 @@ const PRODUCTION_URL =
 	'https://41aty886e1.execute-api.us-west-1.amazonaws.com/production'
 const STAGING_URL =
 	'https://eszlr18ifi.execute-api.us-west-1.amazonaws.com/staging'
-const LOCAL_URL = 'http://192.168.0.111:8000'
+const LOCAL_URL = 'http://localhost:8000'
 
 export function getBackend() {
 	let { releaseChannel } = Expo.Constants.manifest
