a0871457af2109e85572709336f9699d8cf2bbec
David Mora <davidnmora@gmail.com>
2018-05-18 19:28:23 -0700

    Style back button and

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Text, TouchableOpacity } from 'react-native'
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import NavHeader from 'react-navigation-header-buttons'
 import * as taskActions from '../actions/TaskActions'
+import Colors from "../resources/Colors";
 
 /*
  * Imitates React Navigation default back button but additionally
@@ -11,17 +14,55 @@ import * as taskActions from '../actions/TaskActions'
 // Props received from React Navigation
 class BackButton extends React.Component {
 	render() {
-		const { onPress, tintColor, title } = this.props
+		const styles = StyleSheet.create({
+			container: {
+				display: 'flex',
+				flexDirection: 'row',
+				marginLeft: -4,
+			},
+			textContainer: {
+				marginLeft: -4,
+				marginBottom: 1.5,
+				justifyContent: 'center',
+				alignItems: 'center',
+			},
+			text: {
+				fontSize: 17,
+				color: 'white',
+			},
+		})
+		const { onPress, title } = this.props
 		return (
-			<TouchableOpacity onPress={() => this.handlePress(onPress)}>
-				<Text style={{ color: tintColor }}>{title}</Text>
+			<TouchableOpacity onPress={() => this.handlePress(onPress)} style={styles.container}>
+				{this.backArrow()}
+				<View style={styles.textContainer}>
+					<Text style={styles.text} numberOfLines={1}>{title}</Text>
+				</View>
 			</TouchableOpacity>
 		)
 	}
 
-	handlePress(onPress) {
+	handlePress() {
 		this.props.dispatch(taskActions.fetchRecentTasks())
-		onPress() // Navigate back
+		this.props.onPress() // Navigate back
+	}
+
+	backArrow() {
+		return (
+			<NavHeader IconComponent={Ionicons} size={25} color={Colors.white}>
+				<NavHeader.Item
+					iconName="ios-arrow-back"
+					label="Back"
+					onPress={() => this.handlePress()} // Arrow fn binds this context
+					buttonStyle={{
+						fontSize: 34,
+						marginHorizontal: 0,
+						marginLeft: 13,
+						marginRight: 4,
+					}}
+				/>
+			</NavHeader>
+		)
 	}
 }
 
